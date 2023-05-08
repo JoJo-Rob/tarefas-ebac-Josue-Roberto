f3d5df2640b186d490d91d46e85bd61ecbdbf962
Josué Roberto Santana Gomes <130787914+jojorsg@users.noreply.github.com>
2023-05-08 08:30:56 -0300

    Delete mod15 directory

--- mod15/Fusca.java
+++
@@ -1,5 +0,0 @@
-public class Fusca extends Car{
-    public Fusca (int horsePower, String fuelSource, String color) {
-        super(horsePower, fuelSource, color);
-    }
-}
--- mod15/Gol.java
+++
@@ -1,6 +0,0 @@
-public class Gol extends Car{
-
-    public Gol(int horsePower, String fuelSource, String color) {
-        super(horsePower, fuelSource, color);
-    }
-}
