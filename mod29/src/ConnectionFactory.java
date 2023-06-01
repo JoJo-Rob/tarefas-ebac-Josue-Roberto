da202863aec2f44e894f148450d4e273f6d45416
Josué Roberto Santana Gomes <130787914+jojorsg@users.noreply.github.com>
2023-06-01 11:13:37 -0300

    Add files src/main

---
+++ mod29/src/ConnectionFactory.java
@@ -0,0 +1,29 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+
+    private static Connection connection;
+
+    private ConnectionFactory(Connection connection) {
+
+    }
+
+    public static Connection getConnection() throws SQLException {
+        if (connection == null) {
+            connection = initConnection();
+        } else if (connection != null && connection.isClosed()) {
+            connection = initConnection();
+        }
+        return connection;
+    }
+
+    private static Connection initConnection() {
+        try {
+            return DriverManager.getConnection("jdbc:postgresql://localhost:5432/TestJDBC", "postgres", "admin!23");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
